2277ac646f854c38d0e1ff7685c993a4a5216f8e
shobna111 <128703103+shobna111@users.noreply.github.com>
2025-07-14 16:19:02 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,11 @@
-package com.example;
+package com.mycompany.app;
 
 public class App {
+    public String getMessage() {
+        return "Hello World!";
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println(new App().getMessage());
     }
 }
